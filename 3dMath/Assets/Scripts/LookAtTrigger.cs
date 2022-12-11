cb14cd8327889722a0a85e9a4fd3c71a9edc118f
Abdelhakim mohamed <kemstroarab@gmail.com>
2022-12-11 22:27:02 +0200

    .

--- 3dMath/Assets/Scripts/LookAtTrigger.cs
+++ 3dMath/Assets/Scripts/LookAtTrigger.cs
@@ -6,26 +6,31 @@ public class LookAtTrigger : MonoBehaviour
 {
     public Transform p1;
     public Transform p2;
-    ss
+    
     public Transform p3;
     public Transform p4;
     private void OnDrawGizmos()
     {
-        Vector2 p1ToP2Dir = (p2.position - p1.position).normalized;
-        Gizmos.DrawLine(p1.position, (Vector2)p1.position+ p1ToP2Dir);
+        Vector2 p1Pos = p1.position;
+        Vector2 p2Pos = p2.position;
+        Vector2 p3Pos = p3.position;
+        Vector2 p4Pos = p4.position;
 
-        Vector2 p3ToP4Dir = (p4.position - p3.position).normalized;
-        Gizmos.DrawLine(p3.position, (Vector2)p3.position+ p3ToP4Dir);
 
-        float dotProduct= p1ToP2Dir.x * p3ToP4Dir.x + p1ToP2Dir.y * p3ToP4Dir.y;
+        Vector2 p1Top2Dir = p2.position - p1.position;
+        Vector2 p3Top4Dir = p4.position - p3.position;
+
+
+        Vector2 p1Top2Normalized = p1Top2Dir.normalized;
+        Vector2 p3Top4Normalized = p3Top4Dir.normalized;
+
+
+        Gizmos.DrawLine(p1.position, (Vector2)p1.position+ p1Top2Normalized);
+        Gizmos.DrawLine(p3.position, (Vector2)p3.position+ p3Top4Normalized);
+
+        float dotProduct= p1Top2Normalized.x * p3Top4Normalized.x + p1Top2Normalized.y * p3Top4Normalized.y;
 
         Debug.Log(dotProduct);
-
-        
-
-
-       
-
 
     }
 }
